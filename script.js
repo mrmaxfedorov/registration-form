40ea3eb21fe6b0caaad8570e60d78031476205d1
Maxim Fedorov <maxik64@gmail.com>
2022-12-09 23:07:12 +0300

    restoring the sign up project

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const input = document.querySelector('#check')
 const checkboxReg = document.querySelector('#check')
 const checkNull = document.querySelector('#check-null')
 
-const starOne = document.querySelector('.starOne')
-const starTwo = document.querySelector('.starTwo')
-const starThree = document.querySelector('.starThree')
+const asteriskOne = document.querySelector('.asteriskOne')
+const asteriskTwo = document.querySelector('.asteriskTwo')
+const asteriskThree = document.querySelector('.asteriskThree')
 
 const formBottom = document.querySelector('.form__bottom') 
 
@@ -48,23 +48,23 @@ const registrationValidation = () => {
     emailInvalid.style.display = 'none'
     emailReg.style.borderColor = '#CB2424'
     emailName.style.color = '#CB2424'
-    starOne.style.color = '#CB2424'
+    asteriskOne.style.color = '#CB2424'
     emailValid = false
   } else {
     emailRegNull.style.display = 'none'
     emailReg.style.borderColor ='#787878'
     emailName.style.color = '#787878'
-    starOne.style.color = 'black'
+    asteriskOne.style.color = 'black'
     if (!validateEmail(emailAddress)) {
       emailInvalid.style.display = 'block'
       emailReg.style.borderColor = '#CB2424'
       emailName.style.color = '#CB2424'
-      starOne.style.color = '#CB2424'
+      asteriskOne.style.color = '#CB2424'
     } else {
       emailInvalid.style.display = 'none'
       emailReg.style.borderColor ='#787878'
       emailName.style.color = '#787878'
-      starOne.style.color = 'black'
+      asteriskOne.style.color = 'black'
       users.email = emailAddress
       emailValid = true
     }
@@ -74,38 +74,38 @@ const registrationValidation = () => {
     passwordReg.style.borderColor = '#CB2424'
     passwordName.style.color = '#CB2424'
     passwordLength.style.display = 'none'
-    starTwo.style.color = '#CB2424'
-    starTwo.style.margin = '-6px'
+    asteriskTwo.style.color = '#CB2424'
+    asteriskTwo.style.margin = '-6px'
     passwordValid = false
   } else {
     passwordNull.style.display = 'none'
     passwordReg.style.borderColor ='#787878'
     passwordName.style.color ='#787878'
-    starTwo.style.color = 'black'
+    asteriskTwo.style.color = 'black'
     if (password.length < 8) {
       passwordLength.style.display = 'block'
       passwordReg.style.borderColor = '#CB2424'
       passwordName.style.color = '#CB2424'
-      starTwo.style.color = '#CB2424'
-      starTwo.style.margin = '-6px'
+      asteriskTwo.style.color = '#CB2424'
+      asteriskTwo.style.margin = '-6px'
     } else {
       passwordLength.style.display = 'none'
       passwordReg.style.borderColor = '#787878'
       passwordName.style.color = '#787878'
-      starTwo.style.color = 'black'
-      starTwo.style.margin = '-6px'
+      asteriskTwo.style.color = 'black'
+      asteriskTwo.style.margin = '-6px'
       users.password = password
       passwordValid = true
     }
   }
   if (!checkbox) {
     checkNull.style.display = 'block'
-    starThree.style.color = '#CB2424'
+    asteriskThree.style.color = '#CB2424'
     formBottom.style.margin = '0px'
     checkboxValid = false
   } else {
     checkNull.style.display = 'none'
-    starThree.style.color = 'black'
+    asteriskThree.style.color = 'black'
     formBottom.style.margin = '0px'
     checkboxValid = true
   }
@@ -121,6 +121,6 @@ const validationSuccessful = () => {
 
 
 const validateEmail = (email) => {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
